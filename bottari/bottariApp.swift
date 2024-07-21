f82a4a7d2c98e23bcae40cb2fdbf1ee2ad31e667
Eric <real.dassy@gmail.com>
2024-07-21 14:52:53 +0900

    First Commit

---
+++ bottari/bottariApp.swift
@@ -0,0 +1,17 @@
+//
+//  bottariApp.swift
+//  bottari
+//
+//  Created by 송지혁 on 7/21/24.
+//
+
+import SwiftUI
+
+@main
+struct bottariApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
