19718f849bdfc5e499f8a91d950a346294539ee2
Eric <real.dassy@gmail.com>
2024-07-22 07:14:25 +0900

    feat: Memory 스키마 추가

---
+++ bottari/Domain/Entities/Memory.swift
@@ -0,0 +1,18 @@
+//
+//  Memory.swift
+//  bottari
+//
+//  Created by 송지혁 on 7/21/24.
+//
+
+import Combine
+import Foundation
+
+struct Memory {
+    let date: Date
+    let image: String?
+    let title: String
+    let content: String
+    let isBookmarked: Bool
+}
+
