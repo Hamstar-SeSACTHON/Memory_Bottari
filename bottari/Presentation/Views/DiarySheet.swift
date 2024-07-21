d5df823781561e82473cdca65ecf1181d992d99a
Eric <real.dassy@gmail.com>
2024-07-22 07:30:07 +0900

    test: OpenAI Assistant API 동작 테스트를 위한 임시 UI
    API Call 테스트 성공적으로 완료하였고, 빠른 시일 내에 디자인 적용 예정

---
+++ bottari/Presentation/Views/DiarySheet.swift
@@ -0,0 +1,56 @@
+//
+//  DiarySheet.swift
+//  bottari
+//
+//  Created by 송지혁 on 7/22/24.
+//
+
+import SwiftUI
+
+struct DiarySheet: View {
+    @State private var content = ""
+    @ObservedObject var viewModel: DiarySheetViewModel
+    
+    init(viewModel: DiarySheetViewModel) {
+        self._viewModel = ObservedObject(initialValue: viewModel)
+    }
+    
+    var body: some View {
+        TextField("일기를 입력해주세요", text: $content)
+        
+        Button("스레드 완료") {
+            viewModel.createThread(messages: [content])
+        }
+        
+        Button("메시지 완료") {
+            viewModel.createMessage(threadID: viewModel.apiResponse?.id ?? "0", message: """
+                                    2023.08.01
+                                    
+                                    로렌조와 나는 팀이 되어 해커톤에 나갔다.
+                                    미리 잘 준비해서 3일만에 다 만들고 대상을 받았다.
+                                    하지만 아뿔싸 로렌조가 상금을 몽땅 차지하여 달아나버렸다.
+                                    """)
+        }
+        
+        Button("런 완료") {
+            viewModel.createRun(threadID: viewModel.apiResponse?.id ?? "0", assistantID: "asst_ubDWBZLsXJYfBPu1QLBUM5v7")
+        }
+        
+        Button("런스텝 완료") {
+            viewModel.listRunStep(threadID: viewModel.apiResponse?.id ?? "0", runID: viewModel.runResponse?.id ?? "0")
+        }
+        
+        Button("메세지 검색 완료") {
+            viewModel.retrieveMessage(threadID: viewModel.apiResponse?.id ?? "0", messageID: viewModel.runStepResponse?.data.first?.stepDetails.messageCreation?.messageId ?? "0")
+        }
+        
+        Text(viewModel.apiResponse?.id ?? "스레드 nil")
+        Text(viewModel.messageResponse?.id ?? "메시지 nil")
+        Text(viewModel.runResponse?.id ?? "런 nil")
+        Text(viewModel.runStepResponse?.data.first?.stepDetails.messageCreation?.messageId ?? "런 스텝 nil")
+        Text(viewModel.retrieveMessageResponse?.content.first?.text.value ?? "메세지 검색 nil")
+        
+        
+        Text(viewModel.errorMessage ?? "에러 메시지")
+    }
+}
---
+++ bottari/Presentation/Views/MainView.swift
@@ -0,0 +1,38 @@
+//
+//  MainView.swift
+//  bottari
+//
+//  Created by 송지혁 on 7/21/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    @State private var isFloatingButtonClicked = false
+    @EnvironmentObject var container: DIContainer
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "plus")
+                .font(.system(size: 30))
+                .foregroundStyle(.white)
+                .padding()
+                .background {
+                    Circle()
+                        .fill(.blue)
+                }
+                .onTapGesture {
+                    isFloatingButtonClicked.toggle()
+                }
+        }
+        .sheet(isPresented: $isFloatingButtonClicked) {
+            if let createThreadUseCase = container.resolve(CreateThreadUseCase.self), let createMesssageUseCase = container.resolve(CreateMessageUseCase.self), let createRunUseCase = container.resolve(CreateRunUseCase.self), let listRunStepUseCase = container.resolve(ListRunStepUseCase.self), let retrieveMessageUseCase = container.resolve(RetrieveMessageUseCase.self)  {
+                DiarySheet(viewModel: DiarySheetViewModel(createThreadUseCase: createThreadUseCase, createMessageUseCase: createMesssageUseCase, createRunUseCase: createRunUseCase, listRunStepUseCase: listRunStepUseCase, retrieveMessageUseCase: retrieveMessageUseCase))
+            }
+        }
+    }
+}
+
+#Preview {
+    MainView()
+}
