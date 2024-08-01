c475c2422a54ea78972014ace5bf34d9319bbf28
Eric <real.dassy@gmail.com>
2024-08-01 11:10:54 +0900

    test: ImagePicker 기능 테스트

--- bottari/Presentation/Views/DiarySheet.swift
+++ bottari/Presentation/Views/DiarySheet.swift
@@ -10,21 +10,38 @@ import SwiftUI
 struct DiarySheet: View {
     @State private var content = ""
     @ObservedObject var viewModel: DiarySheetViewModel
+    @State private var isPickerPresented = false
+    @StateObject var imagePickerViewModel = ImagePickerViewModel()
     
     init(viewModel: DiarySheetViewModel) {
         self._viewModel = ObservedObject(initialValue: viewModel)
     }
     
     var body: some View {
-        TextField("일기를 입력해주세요", text: $content)
-        
-        Button("전송") {
-            viewModel.sendMessage(content)
+        VStack {
+            TextField("일기를 입력해주세요", text: $content)
+            
+            Button("전송") {
+                viewModel.sendMessage(content)
+            }
+            
+            if let uiImage = imagePickerViewModel.selectedImage {
+                Image(uiImage: uiImage)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+            }
+            
+            Button("Image Picker Test") {
+                isPickerPresented.toggle()
+            }
+            
+            Text(viewModel.receivedMessage ?? "메세지 검색 nil")
+            Text(viewModel.errorMessage ?? "에러 메시지")
         }
-        
-        
-        
-        Text(viewModel.receivedMessage ?? "메세지 검색 nil")
-        Text(viewModel.errorMessage ?? "에러 메시지")
+        .sheet(isPresented: $isPickerPresented) {
+            ImagePicker(isPresented: $isPickerPresented, viewModel: imagePickerViewModel
+            )
+        }
     }
 }
